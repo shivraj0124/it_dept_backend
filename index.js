481cbd54809daaad05330ce65d4ff8e01f590691
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-10-25 10:19:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const url = process.env.API;
 app.use(
   cors({
     // origin: "http://localhost:5173",
-   // origin: "https://information-technology.netlify.app",
-    origin:"*",
+    origin: "https://information-technology.netlify.app",
+    //origin:"*",
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
     optionSuccessStatus: 200,
