a4bb3108826f2b9bfe60870487733052ff46907a
shivraj <sk40fs@gmail.com>
2023-11-11 13:32:25 +0530

    'new

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const url = process.env.API;
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin:'*',
+    // origin:'*',
     origin: "https://information-technology.netlify.app",
     methods: ["POST", "GET", "DELETE", "PUT","HEAD","PATCH"],
     credentials: true,
